934af0bdd0d30dfcaacc03b6af80e577b275e41d
Eugen009 <517572360@qq.com>
2014-02-09 21:42:03 +0800

    修改线框的表现

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -7,7 +7,8 @@ var gCurImageInfo = [
 	6, 7, 8
 ];
 
-var gPuzzleNode = null;
+var gPuzzleNode = null;//九宫格的HTML结构
+var gPuzzleDiv = null;//九宫格的内容
 var gBlockSize = [ 400, 400 ];
 var gBorderSize = 3;
 var gImageFile = "file:///E:/picture/art/ref/browser/39607774.jpg";
@@ -22,6 +23,7 @@ function ToStyleValue( value )
 
 function SetStyleBorder( node, width, type, color )
 {
+	// if ( node.style == null ) return;
 	var valueStr = "";
 	valueStr = "";
 	valueStr += gBorderSize;
@@ -38,6 +40,45 @@ function SetStyleBorder( node, width, type, color )
 	node.style.border = valueStr;
 }
 
+function OnBlockOver( e )
+{
+	if( e != null && e.currentTarget )
+	{
+		SetStyleBorder( e.currentTarget, gBorderSize, "solid", "red" );
+	}
+}
+
+function OnBlockOut( e )
+{
+	if( e != null && e.currentTarget )
+	{
+		SetStyleBorder( e.currentTarget, gBorderSize, "solid", "black" );
+	}
+}
+
+function SetNodeVisible( node, visible )
+{
+	// if( block == null ) return;
+	// var node = block.node;
+	if( !node ) return;
+	if( visible )
+	{
+		node.style.display = "block";
+	}
+	else
+	{
+		node.style.display = "none";
+	}
+}
+
+function CreatePuzzleBlock( id, node )
+{
+	var block = new Object();
+	block.id = id;
+	block.node = node;
+	return block;
+}
+
 function CreatePuzzle( parentNode, imageName )
 {
 	if( parentNode == null )
@@ -45,6 +86,7 @@ function CreatePuzzle( parentNode, imageName )
 	//test only
 	var count = 9;
 	gPuzzleNode = new Array();
+	gPuzzleDiv = new Array();
 	for( var i = 0; i < count; i ++ )
 	{
 		var x = Math.floor(i % 3);// * gBlockSize[0];
@@ -70,25 +112,51 @@ function CreatePuzzle( parentNode, imageName )
 		divNode.style.padding = "0px";
 		divNode.style.overflow = "hidden";
 		SetStyleBorder( divNode, gBorderSize, "solid", "black" );
-		// divNode.onmouseover = "this.style.border = \"3px solid red\""
-
+		if( i < count -1 )
+		{
+			divNode.onmouseover = OnBlockOver;
+			divNode.onmouseout = OnBlockOut;
+		}
 		var imgNode = document.createElement("img");
 		imgNode.src = imageName;
 		imgNode.style.top = ToStyleValue( -y * gBlockSize[1] );
 		imgNode.style.left = ToStyleValue( -x * gBlockSize[0] );
 		imgNode.style.position = "relative";
-		// imgNode.width = 1854;
-		// imgNode.height = 1306;
-		// SetStyleBorder( imgNode, gBorderSize, "solid", "black" );
 		
 		var aNode = document.createElement( "a" );
 		aNode.href = "#";
 		
+		//构成HTML的结构
 		aNode.appendChild( imgNode );
 		divNode.appendChild( aNode );
 		parentNode.appendChild( divNode );
-		gPuzzleNode.push( divNode );
+		
+		//程序结构
+		var puzzleBlock = CreatePuzzleBlock( i, imgNode );
+		gPuzzleNode.push( puzzleBlock );
+		gPuzzleDiv.push( divNode ); 
 	}
+	SetNodeVisible( gPuzzleNode[count-1].node, false );
+	// HideImageByNode( gPuzzleDiv[count -1] );
+}
+
+function GetBlockPosition( block )
+{
+	var count = gPuzzleNode.
+	for( var i = 0; i < count; i ++ )
+	{
+	
+	}
+}
+
+function IsNearBlank( block )
+{
+	
+}
+
+function MoveToBlank( node )
+{
+	
 }
 
 var tempNode = document.getElementById( "main" )
